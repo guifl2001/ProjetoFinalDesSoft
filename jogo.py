fedb20c3552946fea638c38982e7d3725824f0b0
guilherme fontana <guifl2001@hotmail.com>
2020-11-09 11:29:24 -0300

    Teste pygame

---
+++ jogo.py
@@ -0,0 +1,34 @@
+# ===== Inicialização =====
+# ----- Importa e inicia pacotes
+import pygame
+
+pygame.init()
+
+# ----- Gera tela principal
+window = pygame.display.set_mode((500, 400))
+pygame.display.set_caption('Jogo da rapa')
+
+# ----- Inicia estruturas de dados
+game = True
+
+# ----- Inicia assets
+font = pygame.font.SysFont(None, 48)
+text = font.render('Salve rapa', True, (0, 0, 255))
+
+# ===== Loop principal =====
+while game:
+    # ----- Trata eventos
+    for event in pygame.event.get():
+        # ----- Verifica consequências
+        if event.type == pygame.QUIT:
+            game = False
+
+    # ----- Gera saídas
+    window.fill((255, 255, 255))  # Preenche com a cor branca
+    window.blit(text, (10, 10))
+
+    # ----- Atualiza estado do jogo
+    pygame.display.update()  # Mostra o novo frame para o jogador
+
+# ===== Finalização =====
+pygame.quit()  # Função do PyGame que finaliza os recursos utilizados
